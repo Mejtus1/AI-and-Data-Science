7e00e8d24ba21b68a3d868feea38a58411d9bc54
Mejtus1 <GitMaster1769@proton.me>
2023-09-08 23:45:22 +0200

    While and For loops

--- Python_for_data_science_IBM.py
+++ Python_for_data_science_IBM.py
@@ -536,6 +536,48 @@ elif album4 == 1991 or album4 == 1993:
     print("Album came out in year 1991 or 1993")
 
 ############################################################################################
+#Loops in Python
+#repeated executions are performed by loops
+
+#What is for loop?
+#The for loop enables you to execute a code block multiple times
+# For loop example
+dates = [1982,1980,1973]
+N = len(dates)
+for i in range(N):
+    print(dates[i])  
+
+#sequence of numbers from 0 to 7
+for i in range(0, 8):
+    print(i)
+
+# Use for loop to change the elements in list
+squares = ['red', 'yellow', 'green', 'purple', 'blue']
+for i in range(0, 5):
+    print("Before square ", i, 'is',  squares[i])
+    squares[i] = 'white'
+    print("After square ", i, 'is',  squares[i])
+
+# Loop through the list and iterate on both index and element value
+squares=['red', 'yellow', 'green', 'purple', 'blue']
+for i, square in enumerate(squares):
+    print(i, square)
+
+#What is while loop
+#keep executing a code block until a certain condition is met
+# While Loop Example
+dates = [1982, 1980, 1973, 2000]
+i = 0
+year = dates[0]
+
+while(year != 1973):    
+    print(year)
+    i = i + 1
+    year = dates[i]
+    
+print("It took ", i ,"repetitions to get out of loop.")
+
+
 ############################################################################################
 ############################################################################################
 ############################################################################################
