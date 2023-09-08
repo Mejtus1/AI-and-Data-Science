2be2f32f9a9b5ad0aaddb2c2496a082b8d6c26ed
Mejtus1 <GitMaster1769@proton.me>
2023-09-08 21:04:11 +0200

    Sets exercises

--- Python_for_data_science_IBM.py
+++ Python_for_data_science_IBM.py
@@ -375,6 +375,38 @@ album_set_1 = {"Bon Apetit", "Last Friday Night", "E.T."}
 album_set_3 = {"Bon Apetit", "Last Friday Night"}
 album_set_3.issubset(album_set1) = True
 
+#SETS EXERCISES
+set1 = {"pop", "rock", "soul", "hard rock", "rock", "R&B", "rock", "disco"}
+set1
+
+# Convert list to set
+album_list = [ "Michael Jackson", "Thriller", 1982, "00:42:19", \
+              "Pop, Rock, R&B", 46.0, 65, "30-Nov-82", None, 10.0]
+album_set = set(album_list)             
+album_set
+
+#Set Operations
+# Sample set
+A = set(["Thriller", "Back in Black", "AC/DC"])
+# Add element to set
+A.add("NSYNC")
+# Try to add duplicate element to the set
+A.add("NSYNC") # nothing will happen as there can be no duplicates in a set
+# Remove the element from set
+A.remove("NSYNC")
+
+#Sets Logic Operations
+album_set1 = set(["Thriller", 'AC/DC', 'Back in Black'])
+album_set2 = set([ "AC/DC", "Back in Black", "The Dark Side of the Moon"])
+print(album_set1, album_set2)
+# intersect of two sets
+intersection = album_set1 & album_set2
+# Find the difference in set1 but not set2
+album_set1.difference(album_set2)  
+# Find the union of two sets
+album_set1.union(album_set2)
+
+
 ############################################################################################
 #Conditions and Branching 
 
