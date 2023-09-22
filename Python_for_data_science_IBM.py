df382ba917a2ad0d2d1619e8993071b68f03f899
Mejtus1 <GitMaster1769@proton.me>
2023-09-22 16:40:38 +0200

    Objects and classes

--- Python_for_data_science_IBM.py
+++ Python_for_data_science_IBM.py
@@ -973,4 +973,88 @@ print(safe_divide(numerator,denominator))
 #1.5
 
 ############################################################################################
+#Classes and objects
+#For this Class explanation we need to import objects library to better understand 
+import matplotlib.pyplot as plt
+%matplotlib inline  
+
+# Create a class Circle
+class Circle(object):
+    
+    # Constructor
+    def __init__(self, radius=3, color='blue'):
+        self.radius = radius
+        self.color = color 
+    
+    # Method
+    def add_radius(self, r):
+        self.radius = self.radius + r
+        return(self.radius)
+    
+    # Method
+    def drawCircle(self):
+        plt.gca().add_patch(plt.Circle((0, 0), radius=self.radius, fc=self.color))
+        plt.axis('scaled')
+        plt.show()  
+
+#Creating an instance of a class Circle
+# Create an object RedCircle
+RedCircle = Circle(10, 'red')
+# dir command to get a list of the object's methods
+dir(RedCircle)
+# Print the object attribute radius
+RedCircle.radius # 10 
+# Set the object attribute radius
+RedCircle.radius = 1
+RedCircle.radius
+
+#We can draw the object by using the method drawCircle():
+RedCircle.drawCircle()
+
+# Use method to change the object attribute radius
+print('Radius of object:',RedCircle.radius)
+RedCircle.add_radius(2)
+print('Radius of object of after applying the method add_radius(2):',RedCircle.radius)
+RedCircle.add_radius(5)
+print('Radius of object of after applying the method add_radius(5):',RedCircle.radius)
+
+######
+#The Rectangle Class
+class Rectangle(object):
+    
+    # Constructor
+    def __init__(self, width=2, height=3, color='r'):
+        self.height = height 
+        self.width = width
+        self.color = color
+    
+    # Method
+    def drawRectangle(self):
+        plt.gca().add_patch(plt.Rectangle((0, 0), self.width, self.height ,fc=self.color))
+        plt.axis('scaled')
+        plt.show()
+        
+# Create a new object rectangle
+SkinnyBlueRectangle = Rectangle(2, 3, 'blue')
+
+# Print the object attribute height
+SkinnyBlueRectangle.height 
+# Print the object attribute width
+SkinnyBlueRectangle.width
+# Print the object attribute color
+SkinnyBlueRectangle.color
+# Use the drawRectangle method to draw the shape
+SkinnyBlueRectangle.drawRectangle()
+
+# Create a new object rectangle
+FatYellowRectangle = Rectangle(20, 5, 'yellow')
+# Print the object attribute height
+FatYellowRectangle.height 
+# Print the object attribute width
+FatYellowRectangle.width
+# Print the object attribute color
+FatYellowRectangle.color
+# Use the drawRectangle method to draw the shape
+FatYellowRectangle.drawRectangle()
+
 ############################################################################################
