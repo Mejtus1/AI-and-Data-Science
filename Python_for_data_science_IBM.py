73a3ba9dd23320cb854aa103c3faa9c7dc9abb82
Mejtus1 <GitMaster1769@proton.me>
2023-10-06 22:35:35 +0200

    Two dimensional arrays

--- Python_for_data_science_IBM.py
+++ Python_for_data_science_IBM.py
@@ -2039,3 +2039,142 @@ print("odd for array2",odd_arr2)
 
 ####################
 # 2D Numpy in Python
+
+# Import the libraries
+import numpy as np 
+import matplotlib.pyplot as plt
+
+# Create a list
+a = [[11, 12, 13], [21, 22, 23], [31, 32, 33]]
+a # [[11, 12, 13], [21, 22, 23], [31, 32, 33]]
+
+# Convert list to Numpy Array
+# Every element is the same type
+A = np.array(a)
+A # array([[11, 12, 13],
+         # [21, 22, 23],
+         # [31, 32, 33]])
+  
+# Show the numpy array dimensions
+A.ndim # 2 
+
+# Show the numpy array size
+A.size # 9 
+
+# Accessing different elements of a Numpy Array
+#array([[11, 12, 13],
+#       [21, 22, 23],
+#       [31, 32, 33]])
+
+# Access the element on the second row and third column
+A[1, 2] # 23 
+
+# Access the element on the second row and third column
+A[1][2] # 23 
+
+# Access the element on the first row and first column
+A[0][0] # 11
+
+# Access the element on the first row and first and second columns
+A[0][0:2] # array([11, 12])
+
+# Access the element on the first and second rows and third column
+A[0:2, 2] # array([13, 23])
+
+# Create a numpy array X
+X = np.array([[1, 0], [0, 1]]) 
+X # array([[1, 0],
+#          [0, 1]])
+
+# Create a numpy array Y 
+Y = np.array([[2, 1], [1, 2]]) 
+Y # array([[2, 1],
+#          [1, 2]])
+
+# Add X and Y
+Z = X + Y
+Z # array([[3, 1],
+#          [1, 3]])
+
+#Multiply
+
+# Create a numpy array Y
+Y = np.array([[2, 1], [1, 2]]) 
+Y # array([[2, 1],
+#          [1, 2]])
+
+# Multiply Y with 2
+Z = 2 * Y
+Z #
+# array([[4, 2],
+#        [2, 4]])
+
+# Create a numpy array Y
+Y = np.array([[2, 1], [1, 2]]) 
+Y # array([[2, 1],
+#          [1, 2]])
+
+# Create a numpy array X
+X = np.array([[1, 0], [0, 1]]) 
+X # array([[1, 0],
+#          [0, 1]])
+
+# Multiply X with Y
+Z = X * Y
+Z # array([[2, 0],
+#          [0, 2]])
+
+# Create a matrix A
+A = np.array([[0, 1, 1], [1, 0, 1]])
+A # array([[0, 1, 1],
+#          [1, 0, 1]])
+
+# Create a matrix B
+B = np.array([[1, 1], [1, 1], [-1, 1]])
+B # array([[ 1,  1],
+#          [ 1,  1],
+#          [-1,  1]])
+
+We use the numpy function dot to multiply the arrays together
+# Calculate the dot product
+Z = np.dot(A,B)
+Z # array([[0, 2],
+#          [0, 2]])
+
+# Calculate the sin of Z
+np.sin(Z)
+# array([[0.        , 0.90929743],
+#        [0.        , 0.90929743]])
+
+# Create a matrix C
+C = np.array([[1,1],[2,2],[3,3]])
+C # array([[1, 1],
+          #[2, 2],
+      #    [3, 3]])
+
+# Get the transposed of C
+C.T # array([[1, 2, 3],
+#           [1, 2, 3]])
+
+# Exercise
+# Consider the following list a, convert it to Numpy Array
+a = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+A = np.array(a)
+A # array([[ 1,  2,  3,  4],
+#          [ 5,  6,  7,  8],
+#          [ 9, 10, 11, 12]])
+
+
+# Calculate the numpy array size
+np.size(A) # 12
+
+# Access the element on the first row and first and second columns
+A[0, 0:2] # array([1, 2])
+
+# Perform matrix multiplication with the numpy arrays A and B
+B = np.array([[0, 1], [1, 0], [1, 1], [-1, 0]])
+C = np.dot(A, B)
+C #array([[ 1,  4],
+#         [ 5, 12],
+#         [ 9, 20]])
+
